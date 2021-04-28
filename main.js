619151604e479c36b4bc89e19ccf696672cf66a3
M@RK <manfredi23251@lcsd2kids.org>
2021-04-28 09:57:11 -0600

    learning

---
+++ main.js
@@ -0,0 +1,16 @@
+// your javascript file
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = "hey, I'm red";
+
+container.appendChild(content);
+
+const container = document.querySelector('#container');
+
+const content = document.createElement('h3');
+content.classList.add('content2');
+content.textContent = "hey, I'm blue";
+
+container.appendChild(content2);
