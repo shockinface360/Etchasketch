26c52b83e16b9e52417b924cc28e19e6f5bc249b
M@RK <manfredi23251@lcsd2kids.org>
2021-05-06 10:44:45 -0600

    its working

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 
    const gridcontainer =document.querySelector("#grid-container");
-   let grid = 10;
+   let grid = 20;
+ 
 
-for (let index = 0; index < grid; index++) {
+   for (let index2 = 0; index2 < grid; index2++) {
+       let rowCon = document.createElement("div")
+       
+        for (let index = 0; index < grid; index++) {
 
-const singlecell = document.createElement("div");
-singlecell.textContent = ".";
-singlecell.style.color = "black";
-singlecell.classList.add("bg-red");
-singlecell.addEventListener(
-    "mouseover", () => {
-        singlecell.classList.add("bg-blue");
+        const singlecell = document.createElement("div");
+        singlecell.textContent = " .";
+        singlecell.style.color = "bg-red";
+        singlecell.classList.add("bg-red");
+        singlecell.addEventListener(
+            "mouseover", () => {
+                singlecell.classList.add("bg-blue");
+                singlecell.style.color = "bg-blue";
+            }
+        );
+        rowCon.appendChild(singlecell)
+        }
+
+        gridcontainer.appendChild(rowCon);
     }
-);
-gridcontainer.appendChild(singlecell);
-
-}
